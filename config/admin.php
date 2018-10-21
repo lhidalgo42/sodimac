638100d32f409ec01072bc10de849e036d2dc887
lhidalgo42 <lhidalgosep@gmail.com>
2018-10-21 21:28:10 +0100

    hola

--- config/admin.php
+++ config/admin.php
@@ -289,6 +289,15 @@ return [
     |
     */
     'extensions' => [
+        'daterangepicker' => [
 
+            // Set to `false` if you want to disable this extension
+            'enable' => true,
+
+            // Find more configurations http://www.daterangepicker.com/
+            'config' => [
+
+            ]
+        ]
     ],
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,63 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
 
-        <title>Laravel</title>
+    <div class="container">
+        <table class="table table-hover table-bordered" id="taxis">
+            <thead>
+            <tr>
+                <td>Hora de Salida</td>
+                <td>Hora de LLegada</td>
+                <td>Destino</td>
+                <td>Cupos</td>
+                <td>opciones</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>9:00</td>
+                <td>9:50</td>
+                <td>Renca</td>
+                <td>3/4</td>
+                <td>
+                    <button class="btn btn-primary">Subirse al taxi</button>
+                </td>
+            </tr>
+            <tr>
+                <td>9:30</td>
+                <td>10:10</td>
+                <td>Renca</td>
+                <td>3/4</td>
+                <td>
+                    <button class="btn btn-primary">Subirse al taxi</button>
+                </td>
+            </tr>
+            <tr>
+                <td>9:40</td>
+                <td>10:20</td>
+                <td>Renca</td>
+                <td>3/4</td>
+                <td>
+                    <button class="btn btn-primary">Subirse al taxi</button>
+                </td>
+            </tr>
+            <tr>
+                <td>10:00</td>
+                <td>10:50</td>
+                <td>Renca</td>
+                <td>3/4</td>
+                <td>
+                    <button class="btn btn-primary">Subirse al taxi</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
+@section('css')
+    <style>
+        #taxis > tr > td{
+            text-align: center;
+        }
+    </style>
+@endsection
