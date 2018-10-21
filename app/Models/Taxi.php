2275c2029a2102d96a1440409cf43f84b55f0fc5
lhidalgo42 <lhidalgosep@gmail.com>
2018-10-21 22:54:16 +0100

    sql

---
+++ app/Models/Taxi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Taxi extends Model
+{
+    public function users(){
+        return $this->BelongsToMany(User::class);
+    }
+}
