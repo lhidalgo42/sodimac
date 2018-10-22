278fda113cb1005414135944d8b417eb2aad906b
lhidalgo42 <lhidalgosep@gmail.com>
2018-10-22 01:14:30 +0100

    crud tickets

--- database/migrations/2018_10_21_225522_create_tickets_table.php
+++ database/migrations/2018_10_21_225522_create_tickets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration
+class CreateTicketsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('description');
+            $table->integer('user_id');
+            $table->integer('admin_id')->comment('El usuario que autoriza');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('tickets');
     }
 }
---
+++ resources/views/tickets/modals/delete.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leoo
+ * Date: 22-10-18
+ * Time: 0:01
+ */
