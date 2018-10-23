e93dc45bc9b07ea1b45597507f575f42f7b3149f
lhidalgo42 <lhidalgosep@gmail.com>
2018-10-23 07:33:11 +0100

    cambios varios

--- app/Http/Controllers/TaxiController.php
+++ app/Http/Controllers/TaxiController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use Illuminate\Http\Request;
 
 class TaxiController extends Controller
 {
     public function create(){
+        $locations = Location::all();
         return view('taxi.create');
     }
 }
