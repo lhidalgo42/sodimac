7157ddb64aae3aa5ca7816dde2a19a43eb3b80c2
Xmax2997 <maxqm.2997@gmail.com>
2018-11-19 21:30:52 -0300

    interfacelimpia

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -9,7 +9,7 @@
     <div id="app" v-cloak>
         <div class="login-box">
             <div class="login-logo">
-                <a href="{{ url('/home') }}"><b>Admin</b>LTE</a>
+                <a href="{{ url('/home') }}"><span class="logo-lg"><img src="/img/logoweb.png" width="100%" ></span></a>
             </div><!-- /.login-logo -->
 
         @if (count($errors) > 0)
