56ce88cc08b7a5b953acc94cfbfb45ddf3d0148e
Xmax2997 <maxqm.2997@gmail.com>
2018-11-19 22:48:20 -0300

    mascambiosmax

--- resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- To the right -->
+    <div class="pull-right hidden-xs">Aproximadamente se emite 1 Kg de C02 por cada 10 kilometros de viaje en automóvil.</div>
     
     <!-- Default to the left -->
     <strong>Copyright &copy; 2018 <a href="http://www.uai.cl"> uai.cl</a> </strong> {{ trans('adminlte_lang::message.createdby')  }} un grupo de alumnos de la Universidad Adolfo Ibáñez.
