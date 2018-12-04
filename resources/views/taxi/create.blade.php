2ff575bd1a24c7a28c22db3a676ac0ead32b0cbc
lhidalgo42 <lhidalgosep@gmail.com>
2018-12-04 14:30:12 -0300

    se agrega taxi grande

--- resources/views/taxi/create.blade.php
+++ resources/views/taxi/create.blade.php
@@ -11,12 +11,23 @@
             <form action="/taxi/create" method="post">
                 @csrf
                 <div class="form-group">
+                    <label for="origen">Tipo de Taxi</label>
+                    <select class="form-control" id="tipo" name="tipo">
+                        <option value="4" selected>Taxi Normal (4 Cupos)</option>
+                        <option value="7">Taxi Grande (7 Cupos)</option>
+                    </select>
+                </div>
+                <div class="form-group">
                     <label for="origen">N° de Pasajeros</label>
                     <select class="form-control" id="pasajeros" name="pasajeros">
                         <option value="1" selected>1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
-                        <option value="4">4 [Taxi Completo]</option>
+                        <option value="4" class="tc">4 [Taxi Completo]</option>
+                        <option value="4" class="hide tg">4</option>
+                        <option value="5" class="hide tg">5</option>
+                        <option value="6" class="hide tg">6</option>
+                        <option value="7" class="hide tg">7 [Taxi Completo]</option>
                     </select>
                 </div>
                 <div class="form-group">
@@ -53,6 +64,19 @@
 
     <script>
         $(function () {
+            $("#tipo").change(function () {
+                var pasajeros = $("#pasajeros");
+                if ($(this).val() == 7) {
+                    $(".tg").removeClass('hide');
+                    $(".tc").addClass('hide');
+                } else {
+                    $(".tg").addClass('hide');
+                    $(".tc").removeClass('hide');
+                    if (pasajeros.val() > 4) {
+                        pasajeros.val(4);
+                    }
+                }
+            });
             $('#date').datetimepicker({
                 inline: true,
                 sideBySide: true,
@@ -62,25 +86,27 @@
 
             });
             $('#date').data("DateTimePicker").maxDate(moment().endOf('week'));
-            $('#date').on('dp.change', function(e){ $("#realdate").val(e.date.format("YYYY-MM-DD HH:mm:ss")); });
+            $('#date').on('dp.change', function (e) {
+                $("#realdate").val(e.date.format("YYYY-MM-DD HH:mm:ss"));
+            });
             $.ajax({
-                url:"/api/locations",
-                type:"get",
-                success:function (data) {
-                    $.each(data, function(i, item) {
-                        $('#origen').append('<option value="'+item.id+'">'+item.name+' ['+item.address+']</option>');
+                url: "/api/locations",
+                type: "get",
+                success: function (data) {
+                    $.each(data, function (i, item) {
+                        $('#origen').append('<option value="' + item.id + '">' + item.name + ' [' + item.address + ']</option>');
                     });
                 }
             });
             $('#origen').change(function () {
                 $("#destino").html("");
                 $.ajax({
-                    url:"/api/locations/"+$(this).val(),
-                    type:"get",
-                    success:function (data) {
+                    url: "/api/locations/" + $(this).val(),
+                    type: "get",
+                    success: function (data) {
                         $('#destino').append('<option value="">Seleccione Destino</option>');
-                        $.each(data, function(i, item) {
-                            $('#destino').append('<option value="'+item.id+'">'+item.name+' ['+item.address+']</option>');
+                        $.each(data, function (i, item) {
+                            $('#destino').append('<option value="' + item.id + '">' + item.name + ' [' + item.address + ']</option>');
                         });
                     }
                 })
@@ -93,5 +119,7 @@
             type: 'error'
         });
         @endif
+
+
     </script>
 @endsection
